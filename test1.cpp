bdfca0f1c716feac7ade0d80a9c110ed1f8df99e
Sergio <scuenca06@gmail.com>
2025-02-03 17:15:39 +0100

    lab 1 nearly finished

---
+++ test1.cpp
@@ -0,0 +1,11 @@
+#include <Rcpp.h>
+using namespace Rcpp;
+
+// [[Rcpp::export]]
+double test1() {
+  double a = 1.0 / 81;
+  double b = 0;
+  for (int i = 0; i < 729; ++i)
+    b += a;
+  return b;
+}
